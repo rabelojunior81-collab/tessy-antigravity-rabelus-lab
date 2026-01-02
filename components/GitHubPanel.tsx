c0eea7b7cb06a24b47395834855d63971111fa5b
rabelojunior81-collab <rabelojunior81@gmail.com>
2026-01-01 21:12:04 -0300

    R30: Remove componentes duplicados não utilizados
    Remove 4 arquivos duplicados da raiz de components:
    - OptimizationModal.tsx (usa modals/OptimizeModal.tsx)
    - SavePromptModal.tsx (usa modals/SaveModal.tsx)
    - ShareModal.tsx (usa modals/ShareModal.tsx)
    - GitHubPanel.tsx (usa viewers/GitHubViewer.tsx)
    
    Limpeza de duplicatas.

--- components/GitHubPanel.tsx
+++
@@ -1,7 +0,0 @@
-
-import React from 'react';
-
-// Fix: Added repoPath to props to match usage in ProjectDashboard
-const GitHubPanel: React.FC<{ repoPath: string }> = () => null;
-
-export default GitHubPanel;
